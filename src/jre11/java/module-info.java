0375af632528604a1631684fff1fbbb6295405a2
GedMarc <ged_marc@hotmail.com>
2020-11-13 00:40:18 +0200

    module name java.validation to jakarta.validation

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -8,7 +8,7 @@ module com.jwebmp.plugins.smartwizard4 {
 	requires com.jwebmp.core;
 	requires com.guicedee.logmaster;
 
-	requires java.validation;
+	requires jakarta.validation;
 	requires java.logging;
 	requires com.google.guice;
 	requires com.guicedee.guicedinjection;
